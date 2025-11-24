9aa5eab8532107f79a5c6fe5aca75443d852db10
gdhillon2 <gman.dhillon@gmail.com>
2025-11-24 04:33:54 -0800

    iracing worker now passes data dict when it emits signal

--- iracing_worker.py
+++ iracing_worker.py
@@ -8,16 +8,19 @@ class IracingWorker(QObject):
     self.client = IracingClient()
     self.timer = QTimer()
     self.timer.setInterval(poll_rate_ms)
-    self.timer.timeout.connect(self.client.poll_data)
+    self.timer.timeout.connect(self.emit_data)
     self.running = False
 
-  @Slot()
   def run(self):
     self.client.start_connection()
     self.running = True
     self.timer.start()
 
   @Slot()
+  def emit_data(self):
+    if self.running:
+      self.data_ready.emit(self.client.poll_data())
+
   def stop(self):
     self.running = False
     self.client.destroy_connection()
